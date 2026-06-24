9e72fac1631e2eca8ee54c08c8447431cc980ec5
Juan Camilo García Uribe <juancamilogarciauribe@MacBook-Pro-von-Juan.local>
2026-06-23 23:43:20 -0500

    General service created, style improvements

--- src/app/shared/components/organism/upload-garment/upload-garment.ts
+++ src/app/shared/components/organism/upload-garment/upload-garment.ts
@@ -1,34 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { UploadZone } from '../../molecules/upload-zone/upload-zone';
 import { StepperIndicator } from '../../atoms/stepper-indicator/stepper-indicator';
-import { ClothingItem } from '../../../../core/services/interface';
+import { ClothingItem } from '../../../../core/models/interface';
 import { ColorDot } from '../../atoms/color-dot/color-dot';
 import { Badge } from '../../atoms/badge/badge';
 import { SuggestedTags } from '../../atoms/suggested-tags/suggested-tags';
+import { FormsModule } from '@angular/forms';
+import { Icon } from '../../atoms/icon/icons';
+import { ClothingService } from '../../../../core/services/virtual-closet-service';
 
 @Component({
   selector: 'app-upload-garment',
-  imports: [UploadZone, StepperIndicator, Badge, ColorDot, SuggestedTags],
+  imports: [UploadZone, StepperIndicator, Badge, ColorDot, SuggestedTags, Icon, FormsModule],
   templateUrl: './upload-garment.html',
   styleUrl: './upload-garment.css',
 })
 export class UploadGarment {
-  constructor() { }
-  state: 'idle' | 'result' | 'added' = 'idle';
-  aiAnswer: ClothingItem = {} as ClothingItem
+  constructor(public clothingService: ClothingService) {}
   aiPhoto: string = '';
-  
-  getImage(image: string) {
-    this.aiPhoto = `data:image/png;base64,${image}`;
+  editing: boolean = false;
+  editedAiAnswer: ClothingItem = {} as ClothingItem
+
+  onInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    input.style.width = input.value.length + 3 + 'ch';
   }
 
-  changeStatus(newStatus: 'idle' | 'result' | 'added') {
-    this.state = newStatus;
+  toggleEdit() {
+
+    this.editing = !this.editing
+    console.log(this.editedAiAnswer.notes)
   }
-  getText(text: ClothingItem) {
-    this.aiAnswer = text;
-    if (this.aiAnswer) {
-      this.state = 'result';
-    }
+
+  getImage(image: string) {
+    this.aiPhoto = `data:image/png;base64,${image}`;
   }
 }
